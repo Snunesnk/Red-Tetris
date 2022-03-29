4aabc80fb6a1f594601f5b9087a3c148ea15904e
Simon NUNES <snunes@veepee.com>
2022-03-29 15:17:35 +0200

    Added a first store + first dispacth to change cell's color

--- src/client/index.js
+++ src/client/index.js
@@ -1,31 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './store';
 import { App } from './Containers/App/index';
+import store from './Reducers/storeReducer';
 
-console.log("this is a test");
-
-const { io } = require("socket.io-client");
-
-const socket = io();
-
-socket.on("connect", () => {
-    // either with send()  
-    socket.send("Hello Server I'm client!");
-    // or with emit() and custom event names 
-    socket.emit("salutations", "Hello server!", { "mr": "john" }, Uint8Array.from([1, 2, 3, 4]));
-});
-// handle the event sent with socket.send()
-socket.on("message", data => { console.log(data); });
-// handle the event sent with socket.emit()
-socket.on("greetings", (elem1, elem2, elem3) => { console.log(elem1, elem2, elem3); });
-
-socket.on("my-test", () => {
-    console.log("This is my test");
-})
-
-const rootElement = document.getElementById('root');
 ReactDOM.render(
     <Provider store={store}>
         <App />
