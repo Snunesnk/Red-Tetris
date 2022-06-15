ce3c8d6f7e5657e87407fd154bfc36c0f9b69d28
Simon NUNES <snunes@e3r3p8.clusters.42paris.fr>
2022-06-15 16:55:46 +0200

    Created a separate component for the player's pseudo

---
+++ src/client/Components/PlayerPseudo/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Paper } from "@mui/material";
+import { useSelector } from "react-redux";
+import {
+    PseudoPaperStyle,
+    CenteredContainer
+} from "./styles";
+
+export const PlayerPseudoComponent = () => {
+    const appState = useSelector(state => state.appState);
+
+    return (
+        <Paper style={PseudoPaperStyle} elevation={1}>
+            <span>{appState.playerName}</span>
+        </Paper >)
+}
---
+++ src/client/Components/PlayerPseudo/styles.js
@@ -0,0 +1,12 @@
+export const PseudoPaperStyle = {
+    position: "absolute",
+    bottom: "1em",
+    left: "1em",
+    padding: "1.3em",
+}
+
+export const CenteredContainer = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: "end"
+}
