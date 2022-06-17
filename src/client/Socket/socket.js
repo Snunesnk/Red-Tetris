e01816716600065c354b84b7494594df768f0ba4
Simon NUNES <snunes@student.42.fr>
2022-06-17 16:01:17 +0200

    Fix

--- src/client/Socket/socket.js
+++ src/client/Socket/socket.js
@@ -2,7 +2,7 @@ import io from "socket.io-client";
 import setListeners from "./socketListeners";
 
 const PORT = process.env.PORT || 3042
-let socket = io.connect("http://0.0.0.0:" + PORT);
+let socket = io.connect("http://localhost:" + PORT);
 
 export function initSockets(dispatch) {
     setListeners(socket, dispatch);
