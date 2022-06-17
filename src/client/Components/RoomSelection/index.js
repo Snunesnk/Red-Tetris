63be39cfa7a48ba190479568fd02ac1fc6827a3c
Gaston BRADEL <gbradel@e1r8p12.42.fr>
2022-06-17 13:20:56 +0200

    resolve merge conflict

--- src/client/Components/RoomSelection/index.js
+++ src/client/Components/RoomSelection/index.js
@@ -7,26 +7,24 @@ import DialogContent from "@mui/material/DialogContent";
 import TextField from "@mui/material/TextField";
 import { useDispatch, useSelector } from "react-redux";
 import {
-  GridContainerStyle,
-  CreateRoomButtonStyle,
   CenteredContainer,
-  TableHeaderStyle,
   JoinRoomBtnStyle,
-  TableColStyle,
-  TableRowStyle,
   PaperHeaderRowStyle,
   PaperRowStyle,
   DialogBtnContainerStyle,
-  DialogBtn,
+  JoinButtonContainer,
+  GridRow,
+  CreateRoomBtn,
+  HeaderContainer,
+  StartContainer,
 } from "./styles";
 import { LandingInputStyle } from "../Landing/styles";
-
 
 export const RoomSelectionComponent = () => {
   const [open, setOpen] = useState(false);
   const rows = useSelector((state) => state.appState).roomList;
   const dispatch = useDispatch();
-  console.log(rows)
+  console.log(rows);
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -35,9 +33,121 @@ export const RoomSelectionComponent = () => {
   const handleClose = () => {
     setOpen(false);
   };
-  dispatch({ type: "game:list" })
+  dispatch({ type: "game:list" });
   let newRoomName = "";
   const playerName = useSelector((state) => state.appState).playerName;
+
+  return (
+    <Grid container style={CenteredContainer}>
+      {/* Table header */}
+      <Grid item xs={10} md={8} xl={7} style={StartContainer}>
+        <span>Available rooms</span>
+      </Grid>
+      <Grid item xs={10} md={8} xl={7} style={HeaderContainer}>
+        <Grid container>
+          <Grid item xs={10}>
+            <Paper style={PaperHeaderRowStyle}>
+              <Grid container>
+                <Grid item xs={4} md={5} style={CenteredContainer}>
+                  Name
+                </Grid>
+                <Grid item xs={4} md={5} style={CenteredContainer}>
+                  Host
+                </Grid>
+                <Grid item xs={4} md={2} style={CenteredContainer}>
+                  Players
+                </Grid>
+              </Grid>
+            </Paper>
+          </Grid>
+          <Grid item xs={2} style={JoinButtonContainer}>
+            <Button
+              variant="contained"
+              onClick={handleClickOpen}
+              style={CreateRoomBtn}
+            >
+              Create Room
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
+
+      {/* Table rows */}
+      {rows.map((row, i) => (
+        <Grid item xs={10} md={8} xl={7} style={GridRow} key={i}>
+          <Grid container>
+            <Grid item xs={10}>
+              <Paper style={PaperRowStyle} elevation={2}>
+                <Grid container>
+                  <Grid item xs={4} md={5} style={CenteredContainer}>
+                    {row.name}
+                  </Grid>
+                  <Grid item xs={4} md={5} style={CenteredContainer}>
+                    {row.players[0].name}
+                  </Grid>
+                  <Grid item xs={4} md={2} style={CenteredContainer}>
+                    {row.players.length}
+                  </Grid>
+                </Grid>
+              </Paper>
+            </Grid>
+
+            <Grid item xs={2} style={JoinButtonContainer}>
+              <Button
+                variant="contained"
+                style={JoinRoomBtnStyle}
+                onClick={() => {
+                  dispatch({
+                    type: "game:join",
+                    roomName: row.name,
+                    playerName: playerName,
+                  });
+                }}
+              >
+                Join
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      ))}
+
+      <Dialog open={open} onClose={handleClose} fullWidth>
+        <DialogContent>
+          <TextField
+            label="Enter room name"
+            variant="filled"
+            inputProps={{
+              style: LandingInputStyle,
+              onChange: (e) => {
+                newRoomName = e.target.value;
+              },
+            }}
+            fullWidth
+            autoFocus
+          ></TextField>
+        </DialogContent>
+        <DialogActions style={DialogBtnContainerStyle}>
+          <Button variant="contained" onClick={handleClose}>
+            Cancel
+          </Button>
+          <Button
+            variant="contained"
+            // type="submit"
+            onClick={() => {
+              handleClose();
+              dispatch({
+                type: "game:create",
+                roomName: newRoomName,
+                playerName: playerName,
+              });
+            }}
+          >
+            Continue
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </Grid>
+  );
 
   return (
     <Grid container style={GridContainerStyle}>
--- src/client/Components/WaitingRoom/index.js
+++ src/client/Components/WaitingRoom/index.js
@@ -18,6 +18,52 @@ export const WaitingRoomComponent = () => {
     const appState = useSelector(state => state.appState);
     const room = useSelector((state) => state.appState).room;
 
+    console.log(room);
+    return (
+        <Grid container style={GridContainerStyle}>
+
+            <Grid item xs={1} md={3}></Grid>
+            <Grid item xs={10} md={6} style={HeaderPaperContainer}>
+                <Paper style={PlayersHeaderPaperStyle}>
+                    <Grid container>
+                        <Grid item xs={4}></Grid>
+                        <Grid item xs={4} style={CenteredContainer}>Players</Grid>
+                        <Grid item xs={4} style={HeaderSpanContainer}>{room.players.length}/8</Grid>
+                    </Grid>
+                </Paper>
+            </Grid>
+            <Grid item xs={1} md={3}></Grid>
+
+            <Grid item xs={1} md={3}></Grid>
+            <Grid item xs={10} md={6}>
+                <Grid container style={CenteredContainer}>
+                    <Grid item xs={8}>
+                        {room.players.map((player, i) => (
+                            <Paper style={i == 0 ? HostPlayersPaperStyle : PlayersPaperStyle} key={i}>
+                                {player.name}
+                            </Paper>
+                        ))}
+                    </Grid>
+                </Grid>
+            </Grid>
+            <Grid item xs={1} md={3}></Grid>
+
+            <Grid item xs={1} md={3}></Grid>
+            <Grid item xs={10} md={6} style={CenteredContainer}>
+                <Button
+                    variant="contained"
+                    style={PlayButtonStyle}
+                    size="large"
+                    fullWidth
+                    onClick={() => { dispatch({ type: "game:start", roomName: appState.roomName, playerName: appState.playerName }) }}
+                >
+                    Start
+                </Button>
+            </Grid>
+            <Grid item xs={1} md={3}></Grid>
+        </Grid >
+    )
+
     return (
         <Grid container style={GridContainerStyle}>
 
