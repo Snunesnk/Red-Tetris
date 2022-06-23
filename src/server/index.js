15818ee9ca72531f86e3838f73c1c3dfcdb466a4
Simon NUNES <snunes@student.42.fr>
2022-06-23 18:53:03 +0200

    Last try

--- src/server/index.js
+++ src/server/index.js
@@ -15,17 +15,17 @@ function f(value) {
 
 // On connexion to the page, returns the HTML code
 app.get("/", (req, res) => {
-    console.log("path: ")
-    res.sendFile(path.join(__dirname, '../../public', 'index.html'));
+    console.log("path: " + path.resolve(__dirname, '../../public/index.html'));
+    res.sendFile(path.resolve(__dirname, '../../public/index.html'));
 });
 
 // Return the bundle.js, containing all js files
 app.get("/bundle.js", (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public', 'bundle.js'));
+    res.sendFile(path.resolve(__dirname, '../../public/bundle.js'));
 });
 // Return the font
 app.get("/BarcadeBrawl.ttf", (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public', 'BarcadeBrawl.ttf'));
+    res.sendFile(path.resolve(__dirname, '../../public/BarcadeBrawl.ttf'));
 });
 
 const PORT = process.env.PORT || 3042
