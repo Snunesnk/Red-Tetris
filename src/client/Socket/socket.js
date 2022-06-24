af1e21c16231ff45d92d0791b0e72ee5971512df
Simon NUNES <snunes@student.42.fr>
2022-06-24 12:25:45 +0200

    Add debug

--- src/client/Socket/socket.js
+++ src/client/Socket/socket.js
@@ -5,6 +5,8 @@ const PORT = process.env.PORT || 3042
 const domain = PORT == 3042 ? "http://localhost:" + PORT : window.location.hostname;
 let socket = io.connect(domain);
 
+console.log("Domain: " + domain);
+
 export function initSockets(dispatch) {
     setListeners(socket, dispatch);
 }
