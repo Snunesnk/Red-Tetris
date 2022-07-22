00cf51fce99bea4254681e948bb297437863cfd3
Simon NUNES <snunes@student.42.fr>
2022-07-22 14:37:50 +0200

    Fixed a bug where specter where not correctling updating

--- src/server/Socket/InGame/updateSpecter.js
+++ src/server/Socket/InGame/updateSpecter.js
@@ -1,8 +1,14 @@
-function updateSpecter(game, player, socket) {
-  // Create the specter map.
-  // For each column, find the highest piece, and then put everything under at 1
-  let specterMap = JSON.parse(JSON.stringify(player.map));
+const { draw, placeLine, eraseLine } = require("../../tetris/draw");
+
+function getSpecterMap(game, player) {
   let highestFound = [];
+  const piece = game.pieces[player.currentPiece];
+
+  // Erase current piece for the specter map
+  draw(player.map, player.currentPieceX, player.currentPieceY, piece, eraseLine);
+  let specterMap = JSON.parse(JSON.stringify(player.map));
+  draw(player.map, player.currentPieceX, player.currentPieceY, piece, placeLine);
+
   for (let i = 0; i < specterMap.length; i++) {
     for (let j = 0; j < specterMap[i].length; j++) {
       // If there's something, and the column's highest has not been found yet,
@@ -16,6 +22,14 @@ function updateSpecter(game, player, socket) {
     }
   }
 
+  return specterMap;
+}
+
+function updateSpecter(game, player, socket) {
+  // Create the specter map.
+  // For each column, find the highest piece, and then put everything under at 1
+  let specterMap = getSpecterMap(game, player);
+
   socket
     .to(game.name)
     .emit("ingame:updateSpecter", {
@@ -24,4 +38,4 @@ function updateSpecter(game, player, socket) {
     });
 }
 
-module.exports = updateSpecter;
+module.exports = { updateSpecter, getSpecterMap };
