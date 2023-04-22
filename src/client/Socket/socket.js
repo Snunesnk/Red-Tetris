8963f6b9e6eb8894a801f72fb6eac0a470169f9a
Simon NUNES <snunes@veepee.com>
2023-04-22 12:58:36 +0200

    set same port for http and socket

--- src/client/Socket/socket.js
+++ src/client/Socket/socket.js
@@ -4,12 +4,10 @@ import setListeners from "./socketListeners";
 let socket;
 
 if (window.location.hostname === "localhost")
-    socket = io.connect(window.location.hostname + ":3042");
-else
-    socket = io.connect(window.location.hostname);
+  socket = io.connect(window.location.hostname + ":3042");
 
 export function initSockets(dispatch) {
-    setListeners(socket, dispatch);
+  setListeners(socket, dispatch);
 }
 
 export default socket;
